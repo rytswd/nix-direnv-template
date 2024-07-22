790f32dd2823ed1b37791c8323119716b0bbe47f
Ryota <rytswd@gmail.com>
2024-07-22 12:33:14 +0100

    Add root flake for template usage

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Setup using Nix Direnv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { system = system; }; in {
+        # placeholder
+      }
+    ) // {
+      templates.default = {
+        path = ./standard-setup;
+        description = "Standard template for Nix Direnv setup.";
+      };
+    };
+}
